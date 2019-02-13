49d9719e75fe3192c574cf73ac476b0713c46d5d
himankaboruah <himanka.boruah@gmail.com>
2019-02-14 01:59:28 +0530

    Config changes

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+    process.env.PORT = 3000;
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,18 @@
-var mongoose = require('mongoose');
 require('dotenv').config()
+var mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV || 'development';
+var dbname = 'TodoApp';
+
+if (env === 'development') {
+    dbname = 'TodoAppDev';
+} else if (env === 'test') {
+    dbname = 'TodoAppTest';
+}
 
 const user = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
-const dbname = 'TodoApp';
 const uri = `mongodb://${user}:${password}@cluster0-shard-00-00-2hgsc.mongodb.net:27017,cluster0-shard-00-01-2hgsc.mongodb.net:27017,cluster0-shard-00-02-2hgsc.mongodb.net:27017/${dbname}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
 
 mongoose.Promise = global.Promise;
