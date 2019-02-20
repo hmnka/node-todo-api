9ec1a08a07ec793e93ffe8ba81cda1355f1b2725
himankaboruah <himanka.boruah@gmail.com>
2019-02-21 02:23:35 +0530

    Add POST /users/login route

--- server/models/users.js
+++ server/models/users.js
@@ -81,6 +81,28 @@ UserSchema.statics.findByToken = function (token) {
     })
 };
 
+//Model method, 'this' refers to a model
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }                
+            });
+        })
+    });
+};
+
+//Runs before 'save' function
 UserSchema.pre('save', function (next) {
     var user = this;
 
