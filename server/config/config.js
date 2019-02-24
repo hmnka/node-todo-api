51af3c86977d1c6ab19fa028a5b49da7a3f22224
himankaboruah <himanka.boruah@gmail.com>
2019-02-25 02:19:59 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || env === 'test') {
-    process.env.PORT = 3000;
+    var config = require('./config.json');
+    var envConfig = config[env];
+    
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+    // process.env.PORT = 3000;
 }
--- server/models/users.js
+++ server/models/users.js
@@ -50,7 +50,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access: access}, 'abc123').toString();
+    var token = jwt.sign({_id: user._id.toHexString(), access: access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access: access, token: token});
 
@@ -78,7 +78,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         //Same as - return Promise.reject();
         return new Promise((resolve, reject) => {
